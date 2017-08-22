f467969b0d6e9f097dfbd33e7ceddc299d862a70
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2017-08-22 23:42:12 +0530

    Added Data file

---
+++ functions/yogaData.js
@@ -0,0 +1,21 @@
+let yogaData = [
+    {
+        name: "Garland Pose",
+        description: ["first variant, the hands are folded together at the back, while the chin touches the floor",
+            '<break time=“5s”/>',
+            "second variant, the feet are placed at the floor, one takes a squatted position, folds the hands around the heels"
+        ]
+    },
+    {
+        name: "Four-Limbed Staff Pose",
+        description: ["Chaturaṅga Daṇḍāsana the hands and feet are on the floor, supporting the body",
+            '<break time=“3s”/>',
+            "which is parallel to and lowered toward, but not touching, the floor.",
+            '<break time=“3s”/>',
+            "It looks much like a push up, but with the hands quite low (just above the pelvis), and the elbows kept in along the sides of the body"]
+    }
+];
+
+module.export = {
+    yogaData: yogaData
+};
